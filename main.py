b9ef07f856d073fb63f10ded6530e15c16c54609
Ishimov <Tinkitop@yandex.ru>
2022-10-12 09:57:55 +0500

    add csv

--- main.py
+++ main.py
@@ -18,9 +18,11 @@ for i in range (len(objids)):
     url.append(f'https://xn--80az8a.xn--d1aqf.xn--p1ai/%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B/api/object/{idd}')
 
 res = requests.get(url[0])
-res.json()
-print(*url, *res, sep='\n')
+ress = res.json()
+print(*url, sep='\n')
+print(ress)
 # print(len(url), url, sep='\n')
 
-# df = pd.DataFrame(res)
-# print(df)
+df = pd.DataFrame(ress)
+df.to_csv("panda.csv", index=True, header=False)
+print(df)
